c470ec35c57ca68322c70f1920e56e607547358a
Pasha <nikolenko.pavel@gmail.com>
2023-01-17 02:09:59 +0300

    Начальный вариант.

---
+++ src/lambdas/MyPredicate.java
@@ -0,0 +1,10 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class MyPredicate implements Predicate<Integer> {
+    @Override
+    public boolean test(Integer i) {
+        return (i > 0);
+    }
+}
